c317edaee764df4eae2e2f28df4e39b535c7d3e2
nacht-schatten <joh3011@hotmail.de>
2025-07-13 17:41:24 +0200

    Update Schatzsuche.py

--- Schatzsuche.py
+++ Schatzsuche.py
@@ -91,8 +91,7 @@ if level.startswith("Singleplayer"):
     if "geheimes_s" not in st.session_state:
         st.session_state.geheimes_s = random.randint(1, p - 1)
 else:
-   
-    st.session_state.geheimes_s = s
+     st.session_state.geheimes_s = s
 
 
 
