72bbfdcd810f96a1232a080f7f63239736c29873
nacht-schatten <joh3011@hotmail.de>
2025-07-13 17:49:15 +0200

    Update Schatzsuche.py

--- Schatzsuche.py
+++ Schatzsuche.py
@@ -41,6 +41,7 @@ st.markdown("""
     <div class="schreibmaschine">
 Finde alle Hinweise und entdecke das Geheimnis! Nur... welchen Modus sollst du wählen?
 
+
 **Gamemaster-Modus:** Du musst ein bestimmtes Geheimnis für eine Aktion finden.
 
 **Singleplayer-Modus:** Du möchtest dich alleine am Rätseln probieren. Für jedes Spiel wird ein neues Geheimnis erstellt!
